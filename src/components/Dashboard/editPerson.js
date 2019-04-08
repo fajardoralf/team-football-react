83c69ee4eb5357a1b5d7ee62b60771299d872476
Thomas G <thomas_grefsrud@hotmail.com>
2019-04-08 13:11:39 +0200

    Basic scaffold for dashboard

---
+++ src/components/Dashboard/editPerson.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const editPerson = (props) => {
+
+    return (
+        <div>
+            <p>Edit personal info</p>
+        </div>
+    )
+};
+
+export default editPerson;
--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from '../Login/Login';
+import Login from '../../components/Login/Login';
 
 
 class PageNavbar extends React.Component {
