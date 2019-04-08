fed806c878964388207490c4f925000cc2a2b0e6
Stixus <stianheimset@hotmail.com>
2019-04-08 15:34:17 +0200

    Made Dashboard elements scalable on hover

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import CreateLocation from '../../components/Dashboard/createLocation';
 import CreatePerson from '../../components/Dashboard/createPerson';
 import CreateTeam from '../../components/Dashboard/createTeam';
 import EditPerson from '../../components/Dashboard/editPerson';
+import './Dashboard.css';
 
 
 class Dashboard extends React.Component {
@@ -15,19 +16,19 @@ class Dashboard extends React.Component {
             </div>
             <h1>Dashboard</h1>
                 <div className="row">
-                    <div className="col">
+                    <div className="col" id="dashboardElement">
                         <h3 className="mt-5">Edit User</h3>
                         <EditPerson/>
                     </div>
-                    <div className="col">
+                    <div className="col" id="dashboardElement">
                         <h3 className="mt-5">Create Person</h3>
                         <CreatePerson/>
                     </div>
-                    <div className="col">
+                    <div className="col" id="dashboardElement">
                         <h3 className="mt-5">Create Team</h3>
                         <CreateTeam/>
                     </div>
-                    <div className="col">
+                    <div className="col" id="dashboardElement">
                         <h3 className="mt-5">Create Location</h3>
                         <CreateLocation/>
                     </div>
