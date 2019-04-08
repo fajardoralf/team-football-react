3141ba47aa8712162e2c117a2d18680e916170bb
Thomas G <thomas_grefsrud@hotmail.com>
2019-04-08 14:03:21 +0200

    small changes to login/navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './containers/Login/Login'
+import Navbar from './containers/Navbar/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-            <Login/>
-        </div>
+        <Navbar />
       </div>
     );
   }
