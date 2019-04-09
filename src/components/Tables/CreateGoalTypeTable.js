b4c79a0d7bf843e7b6772870d9c873656a23f0fc
Stixus <stianheimset@hotmail.com>
2019-04-09 10:44:22 +0200

    Added CreateGoalTypeTable REACT

---
+++ src/components/Tables/CreateGoalTypeTable.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Form, Button, Card } from "react-bootstrap";
+import axios from "axios";
+
+const URL = "";
+
+class CreateGoalTypeTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: ""
+    };
+  }
+
+  handleForm(event) {
+    event.preventDefault();
+
+    axios
+      .post(URL, {
+        type: this.state.type
+      })
+    this.setState({
+      type: ""
+    });
+  }
+
+  setType(event) {
+    this.setState({ 
+      type: event.target.value
+    });
+  }
+
+  componentDidMount() {
+    axios.get(URL).then(json => this.setState({ store: json.data }));
+  }
+
+  render() {
+    let title = "Create Goal Type"
+
+    return (
+      <Card bg="light" text="black" style={{ width: "18rem" }}>
+        <Card.Body>
+          <h3 className="text-center">{title}</h3>
+          <br />
+          <Form onSubmit={this.handleForm.bind(this)}>
+
+            <Form.Group controlId="createGoalTypeForm">
+              <Form.Label>Type</Form.Label>
+              <Form.Control
+                type="type"
+                placeholder="Type"
+                value={this.state.type}
+                onChange={this.setType.bind(this)}
+              />
+            </Form.Group>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Button variant="dark" type="Submit">
+                Create
+              </Button>
+            </div>
+          </Form>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+export default CreateGoalTypeTable;
