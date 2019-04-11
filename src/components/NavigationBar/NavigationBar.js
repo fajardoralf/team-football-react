29ddc59c43bbc5087453ccb4b9428ea6ba2ee69e
Stixus <stianheimset@hotmail.com>
2019-04-11 11:43:40 +0200

    Added Collapsable Menu, needs more work

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -12,25 +12,25 @@ class NavigationBar extends React.Component {
   render() {
 
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="NavigationBar">
-        <a class="navbar-brand" href="#">Football Manager</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="NavigationBar">
+        <a className="navbar-brand" href="/">Football Manager</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/dashboard">Dashboard</a>
+            <li className="nav-item">
+              <a className="nav-link" href="/dashboard">Dashboard</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
+            <li className="nav-item">
+              <a className="nav-link" href="/">About</a>
             </li>
           </ul>
-          <span class="navbar-text">
-            <a class="nav-link" href="#">Logout</a>
+          <span className="navbar-text">
+            <a className="nav-link" href="/">Logout</a>
           </span>
         </div>
       </nav>
