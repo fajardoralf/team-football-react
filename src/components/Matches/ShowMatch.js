ddd56b83be845c4c8e7f899f29aba989d4544ffb
Thomas G <thomas_grefsrud@hotmail.com>
2019-04-12 14:46:48 +0100

    Moved matches to row, show win/loss for anon

--- src/components/Matches/ShowMatch.js
+++ src/components/Matches/ShowMatch.js
@@ -1,25 +1,33 @@
 import React from 'react'
-import {Card} from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
+import './ShowMatch.css'
 
-class ShowMatch extends React.Component{
+class ShowMatch extends React.Component {
 
     constructor(props) {
         super(props)
 
         this.state = {
-            homeTeam : props.homeTeam,
-            awayTeam : props.awayTeam,
-            result : props.result,
+            homeTeam: props.homeTeam,
+            awayTeam: props.awayTeam,
+            result: props.result,
             role: props.role,
         }
     }
 
     render() {
+        let winner = (this.state.result[0] > this.state.result[1]) ? "home" : (this.state.result[0] === this.state.result[1] ? "tie" : "away")
 
         return (
-            <Card>
+            <Card className="match">
                 <div>{this.state.homeTeam} - {this.state.awayTeam} </div>
-                {(this.state.role) ? <div> {this.state.result[0]} - {this.state.result[1]} </div> : <div>Log in to see result</div>}
+                {(this.state.role) ?
+                    <div> {this.state.result[0]} - {this.state.result[1]} </div>
+                    :
+                     (this.winner !== 'tie') ? <div>Winner: {winner} team! Log in to see score</div> 
+                     :
+                     <div>Tie! Log in to see the score</div> 
+                }
             </Card>
         )
     }
