5a2622ced9883b7bda934d7808a34d041c31d260
fajardoralf <fajardoralf@gmail.com>
2019-04-17 11:26:04 +0200

    create matchgoal

--- src/components/CreateTables/CreateMatchPositionTable.js
+++ src/components/CreateTables/CreateMatchPositionTable.js
@@ -3,11 +3,16 @@ import { Form, Button, Card } from "react-bootstrap";
 import axios from "axios";
 
 const URL = "https://team-football-api.herokuapp.com/matchposition/";
-
+const matchURL = "https://team-football-api.herokuapp.com/match/";
+const playerURL = "https://team-football-api.herokuapp.com/player/";
+const matchpositionURL =
+  "https://team-football-api.herokuapp.com/matchposition/";
 class CreateMatchPositionTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      player: [],
+      match: [],
       player_id: "",
       match_id: "",
       position: "",
@@ -16,17 +21,62 @@ class CreateMatchPositionTable extends React.Component {
     };
   }
 
+  fetchPlayer = () => {
+    axios
+      .get(playerURL, {
+        header: {
+          "Content-Type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+      .then(res => {
+        this.setState({ playerId: res.data[0].player_id });
+        let data = res.data.map(data => {
+          return {
+            key: data.player_id,
+            first_name: data.person.first_name,
+            last_name: data.person.last_name
+          };
+        });
+        this.setState({ player: data });
+      });
+  };
+
+  fetchMatch = () => {
+    axios
+      .get(matchURL, {
+        headers: {
+          "Content-Type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+      .then(res => {
+        this.setState({ matchId: res.data[0].match_id });
+        let data = res.data.map(data => {
+          return {
+            key: data.match_id,
+            date: data.match_date,
+            home_team: data.home_team.team_name,
+            away_team: data.away_team.team_name
+          };
+        });
+        this.setState({ match: data });
+      })
+      .catch(err => {
+        console.log("Axios error: ", err);
+      });
+  };
+
   handleForm(event) {
     event.preventDefault();
 
-    axios
-      .post(URL, {
-        player_id: this.state.player_id,
-        match_id: this.state.match_id,
-        position: this.state.position,
-        message: "Successfully created ",
-        submitted: true
-      })
+    axios.post(URL, {
+      player_id: this.state.player_id,
+      match_id: this.state.match_id,
+      position: this.state.position,
+      message: "Successfully created ",
+      submitted: true
+    });
     this.setState({
       player_id: "",
       match_id: "",
@@ -35,7 +85,7 @@ class CreateMatchPositionTable extends React.Component {
   }
 
   setPlayer_id(event) {
-    this.setState({ 
+    this.setState({
       player_id: event.target.value
     });
   }
@@ -52,34 +102,49 @@ class CreateMatchPositionTable extends React.Component {
     });
   }
 
-  render() {
-    let title = "Create Match-Position"
+  componentDidMount() {
+    this.fetchMatch();
+    this.fetchPlayer();
+  }
 
+  render() {
+    let title = "Create Match-Position";
+    const { player, match } = this.state;
     return (
       <Card bg="light" text="black" style={{ width: "18rem" }}>
         <Card.Body>
           <h3 className="text-center">{title}</h3>
           <br />
           <Form onSubmit={this.handleForm.bind(this)}>
-
             <Form.Group controlId="createMatchPositionForm">
-              <Form.Label>Player ID</Form.Label>
-              <Form.Control
-                type="player_id"
-                placeholder="Player ID"
-                value={this.state.player_id}
-                onChange={this.setPlayer_id.bind(this)}
-              />
+              <Form.Label>Player</Form.Label>
+              <Form.Control onChange={this.setPlayerId} as="select">
+                {player.map(data => {
+                  return (
+                    <option
+                      key={data.key}
+                      value={data.key}
+                      first_name={data.first_name}
+                      last_name={data.last_name}
+                    >
+                      {data.first_name + " " + data.last_name}
+                    </option>
+                  );
+                })}
+              </Form.Control>
             </Form.Group>
 
             <Form.Group controlId="createMatchPositionForm">
-              <Form.Label>Match ID</Form.Label>
-              <Form.Control
-                type="match_id"
-                placeholder="Match ID"
-                value={this.state.match_id}
-                onChange={this.setMatch_id.bind(this)}
-              />
+              <Form.Label>Match</Form.Label>
+              <Form.Control onChange={this.setMatchId} as="select">
+                {match.map(data => {
+                  return (
+                    <option key={data.key} value={data.key}>
+                      {data.date}
+                    </option>
+                  );
+                })}
+              </Form.Control>
             </Form.Group>
 
             <Form.Group controlId="createMatchPositionForm">
