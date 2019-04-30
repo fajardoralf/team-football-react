39aab0b7fa9c6da3c778163adc7b382f2b8ff992
Thomas G <thomas_grefsrud@hotmail.com>
2019-04-30 09:40:11 +0200

    Update self shows username as placeholder

--- src/components/UpdateTables/UpdateSelfTable.js
+++ src/components/UpdateTables/UpdateSelfTable.js
@@ -8,7 +8,7 @@ class UpdateSelfTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      personId: sessionStorage.getItem("user_id"),
+      personId: parseInt(sessionStorage.getItem("user_id")),
       user: {},
       addressId: "",
       firstName: "",
@@ -62,42 +62,9 @@ class UpdateSelfTable extends React.Component {
   }
 
   componentDidMount() {
-    /*
-    axios.get(URL + "person/" + this.state.personId).then(res =>
-      this.setState({
-        addressId: res.data.address_id,
-        firstName: res.data.first_name,
-        lastName: res.data.last_name,
-        dateOfBirth: res.data.date_of_birth
-      })
-    );
-    */
     axios
-
       .get(URL + "users/" + this.state.personId)
       .then(res => this.setState({ user: res.data }));
-    /*
-    axios
-      .get(URL + "address/", {
-        headers: {
-          "Content-Type": "application/json;charset=UTF-8"
-        }
-      })
-      .then(res => {
-        let data = res.data.map(data => {
-          return {
-            key: data.address_id,
-            address_line_1: data.address_line_1
-          };
-        });
-        this.setState({
-          addresses: data
-        });
-      })
-      .catch(err => {
-        console.log("Axios error: ", err);
-      });
-      */
   }
 
   handlePasswordChange = event => {
@@ -106,7 +73,9 @@ class UpdateSelfTable extends React.Component {
       .put(URL + "users/" + this.state.personId, {
         user_id: this.state.personId,
         //username: this.state.user.username,
-        username: this.state.new_username,
+        username: this.state.new_username !== '' 
+          ? this.state.new_username
+          : this.state.user.username,
         password:
           this.state.new_password !== ""
             ? this.state.new_password
@@ -132,84 +101,10 @@ class UpdateSelfTable extends React.Component {
 
   render() {
     const titleInfo = "Update Your Information";
-    const titlePassword = "Update Your Password";
-    const { addressId, addresses } = this.state;
+    const {user} = this.state;
 
     return (
       <div className="row">
-        {/*
-        <div className="col">
-          <Card bg="light" text="black" style={{ width: "100%" }}>
-            <Card.Body>
-              <h3 className="text-center">{titleInfo}</h3>
-              <br />
-              <Form onSubmit={this.handleForm.bind(this)}>
-                <Form.Group controlId="updatePersonForm">
-                  <Form.Label>Address ID</Form.Label>
-                  <Form.Control
-                    type="addressId"
-                    placeholder="Address ID"
-                    value={addressId}
-                    onChange={this.setAddressId.bind(this)}
-                    as="select"
-                  >
-                    {addresses.map((a, index) => {
-                      return (
-                        <option key={index} value={a.address_id}>
-                          {a.address_line_1}
-                        </option>
-                      );
-                    })}
-                  </Form.Control>
-                </Form.Group>
-
-                <Form.Group controlId="udpatePersonForm">
-                  <Form.Label>First Name</Form.Label>
-                  <Form.Control
-                    type="firstName"
-                    placeholder="First Name"
-                    value={this.state.firstName}
-                    onChange={this.setFirstName.bind(this)}
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="updatePersonForm">
-                  <Form.Label>Last Name</Form.Label>
-                  <Form.Control
-                    type="lastName"
-                    placeholder="Last Name"
-                    value={this.state.lastName}
-                    onChange={this.setLastName.bind(this)}
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="updatePersonForm">
-                  <Form.Label>Date of Birth</Form.Label>
-                  <Form.Control
-                    type="dateOfBirth"
-                    placeholder="Date of Birth"
-                    value={this.state.dateOfBirth}
-                    onChange={this.setDateOfBirth.bind(this)}
-                  />
-                </Form.Group>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center"
-                  }}
-                >
-                  <Button variant="dark" type="Submit">
-                    Update
-                  </Button>
-
-                  <div className="text-center">{this.state.message}</div>
-                </div>
-              </Form>
-            </Card.Body>
-          </Card>
-        </div>
-        */}
         <div className="col">
           <Card bg="light" text="black">
             <Card.Body>
@@ -219,7 +114,7 @@ class UpdateSelfTable extends React.Component {
                   <Form.Label>New Username</Form.Label>
                   <Form.Control
                     type="username"
-                    placeholder="New Username"
+                    placeholder={user.username}
                     value={this.state.new_username}
                     onChange={this.setNewUsername}
                   />
